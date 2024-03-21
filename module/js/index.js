40fee7f99b8873699f92dc207ec7e9aaaa5a28fa
maleks1983 <maleks1983@gmail.com>
2024-03-21 22:13:38 +0200

    f

--- module/js/index.js
+++ module/js/index.js
@@ -39,3 +39,49 @@ function handleCredentialResponse(response) {
   // console.log("UIT: " + responsePayload.jti);
 }
 
+function googleLogin() {
+  this.loginFormGoogle = document.getElementById('g_id_onload');
+  this.loggedUser = {
+    googleID: '',
+    name: '',
+    email: '',
+    UIT: ''
+  };
+
+  this.init();
+  this.googleLogin = function init() {
+    this.loginFormGoogle[data - client_id] = "355085333852-8pr4q546m8hcdo2896mrc6ahq2brdaug.apps.googleusercontent.com";
+    this.loginFormGoogle[data - callback] = this.handleCredentialResponse(response);
+
+  }
+
+  this.decodeJwtResponse = function decodeJwtResponse(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload);
+  }
+
+  this.handleCredentialResponse = function handleCredentialResponse(response) {
+    const responsePayload = this.decodeJwtResponse(response.credential);
+    this.loggedUser.googleID = responsePayload.sub;
+    this.loggedUser.name = responsePayload.given_name;
+    this.loggedUser.email = responsePayload.email;
+    this.loggedUser.UIT = responsePayload.jti;
+
+    userName(responsePayload);
+  }
+
+
+}
+const googleSignIn = document.querySelector('.g_id_signin')
+googleSignIn.addEventListener('click', new googleLogin(),)
+const googleLogin = new googleLogin();
