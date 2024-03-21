37a151701149ce7fbb1ba4cbaba559cc48370432
maleks1983 <maleks1983@gmail.com>
2024-03-21 12:37:00 +0200

    first commit

---
+++ module/js/index.js
@@ -0,0 +1,39 @@
+const elementFormLoginDiv = document.querySelector('.form-login');
+const lof= new logInForm();
+elementFormLoginDiv.addEventListener('click', function () {
+    this.append(lof.builder())
+})
+
+
+
+function logInForm (){
+
+    this.builder = function builder() {
+        const elementFormLogin = document.createElement('form');
+        elementFormLogin.apclassList = ('form-signin');
+        elementFormLogin.setAttribute('th:metod', 'POST');
+        elementFormLogin.setAttribute('th:href', '/login');
+        elementFormLogin.innerText = ('Login');
+        return elementFormLogin;
+    }
+
+
+}
+
+function createSecretHolder(secret) {
+  
+    this.secret = secret;
+    
+    this.setSecret = function setSecret(secret){
+      this.secret = secret;
+    }
+    
+    this.getSecret = function getSecret(){
+      return this.secret;
+    }
+    
+  }
+
+const obj = new createSecretHolder(5);
+obj.setSecret(2);
+
