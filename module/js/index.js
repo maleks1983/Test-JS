d7f634220c298c6ac7f7afacc3061b9feee58f75
maleks1983 <maleks1983@gmail.com>
2024-03-21 20:34:55 +0200

    f

--- module/js/index.js
+++ module/js/index.js
@@ -1,11 +1,19 @@
-import jwt from 'jsonwebtoken';
+
 
 // Функція для розкодування JWT-токену та отримання інформації про користувача
-function decodeJwtResponse(credential) {
-  const token = credential.id_token;
-  const decoded = jwt.decode(token);
+function decodeJwtResponse(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-  return decoded;
+  return JSON.parse(jsonPayload);
 }
 
 const elementFormLoginDiv = document.querySelector('.form-login');
