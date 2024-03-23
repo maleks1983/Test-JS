c743e2b72a5ce6f49750636247fd848069eff081
maleks1983 <maleks1983@gmail.com>
2024-03-23 21:13:42 +0200

    modified:   module/js/index.js

--- module/js/index.js
+++ module/js/index.js
@@ -1,19 +1,21 @@
 
 
-  // console.log("ID: " + responsePayload.sub);
-  // console.log('Full Name: ' + responsePayload.name);
-  // console.log('Given Name: ' + responsePayload.given_name);
-  // console.log('Family Name: ' + responsePayload.family_name);
-  // console.log("Image URL: " + responsePayload.picture);
-  // console.log("Email: " + responsePayload.email);
-  // console.log("UIT: " + responsePayload.jti);
+// console.log("ID: " + responsePayload.sub);
+// console.log('Full Name: ' + responsePayload.name);
+// console.log('Given Name: ' + responsePayload.given_name);
+// console.log('Family Name: ' + responsePayload.family_name);
+// console.log("Image URL: " + responsePayload.picture);
+// console.log("Email: " + responsePayload.email);
+// console.log("UIT: " + responsePayload.jti);
 
 
 class GoogleLogin {
 
-  constructor(selector) {
+  constructor(selector, client_id, callback) {
     this.props = {
-      selector
+      selector,
+      client_id,
+      callback
     };
 
     this.rootElement = document.querySelector(selector);
@@ -28,51 +30,46 @@ class GoogleLogin {
       UIT: ''
     };
 
-    this.loginFormGoogle.dataset['client_id'] = "355085333852-8pr4q546m8hcdo2896mrc6ahq2brdaug.apps.googleusercontent.com";
-    this.loginFormGoogle.dataset['callback'] = "handleCredentialResponse";
-    
+    this.loginFormGoogle.dataset['client_id'] = client_id;
+    this.loginFormGoogle.dataset['callback'] = callback;
 
-    function handleCredentialResponse(response) {
-      console.dir(response);
-      this.handleCredentialResponse(response);
+    this.decodeJwtResponse = function (token) {
+      var base64Url = token.split(".")[1];
+      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      var jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+      return JSON.parse(jsonPayload);
+    };
+
+    this.handleCredentialResponse = function (response) {
+      if (response !== null) {
+        const responsePayload = this.decodeJwtResponse(response);
+
+        this.loggedUser.googleID = responsePayload.sub;
+        this.loggedUser.name = responsePayload.given_name;
+        this.loggedUser.email = responsePayload.email;
+        this.loggedUser.UIT = responsePayload.jti;
+        this.elenentUser.onload;
+
+        this.elenentUser.innerText = this.loggedUser.name;
+      }
+
     }
 
   }
 
+};
+
+const gLogin = new GoogleLogin(".container.mx-auto.form-login",
+  "355085333852-8pr4q546m8hcdo2896mrc6ahq2brdaug.apps.googleusercontent.com",
+  "handleCredentialResponse");
+
+function handleCredentialResponse(response) {
+  gLogin.handleCredentialResponse(response);
 }
-
-GoogleLogin.prototype.decodeJwtResponse = function (token) {
-  console.dir(token);
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-  return JSON.parse(jsonPayload);
-};
-
-GoogleLogin.prototype.handleCredentialResponse = function (response) {
-  console.dir(this, response);
-  if (response !== null) {
-    const responsePayload = this.decodeJwtResponse.bind(this, response);
-    this.loggedUser.googleID = responsePayload.sub;
-    this.loggedUser.name = responsePayload.given_name;
-    this.loggedUser.email = responsePayload.email;
-    this.loggedUser.UIT = responsePayload.jti;
-    this.elenentUser.onload;
-
-    this.elenentUser.innerText = this.loggedUser.name;
-  }
-
-
-
-};
-
-const gLogin = new GoogleLogin(".container.mx-auto.form-login");
-
-
