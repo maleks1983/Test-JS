afdfd84fc109370b44613873b785c5a26259f31c
maleks1983 <maleks1983@gmail.com>
2024-03-23 21:03:41 +0200

    modified:   module/js/index.js

--- module/js/index.js
+++ module/js/index.js
@@ -1,27 +1,4 @@
 
-// Функція для розкодування JWT-токену та отримання інформації про користувача
-function decodeJwtResponse(token) {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
-
-
-
-
-function handleCredentialResponse(response) {
-  const responsePayload = decodeJwtResponse(response.credential);
-  userName(responsePayload);
 
   // console.log("ID: " + responsePayload.sub);
   // console.log('Full Name: ' + responsePayload.name);
@@ -30,7 +7,6 @@ function handleCredentialResponse(response) {
   // console.log("Image URL: " + responsePayload.picture);
   // console.log("Email: " + responsePayload.email);
   // console.log("UIT: " + responsePayload.jti);
-}
 
 
 class GoogleLogin {
