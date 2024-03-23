95a777df1f130b1a61471b8e232c5ca151ec4674
maleks1983 <maleks1983@gmail.com>
2024-03-23 19:53:44 +0200

    modified:   module/js/index.js

--- module/js/index.js
+++ module/js/index.js
@@ -41,9 +41,9 @@ class GoogleLogin {
     };
 
     this.rootElement = document.querySelector(selector);
-    this.loginFormGoogle = document.getElementById('g_id_onload');
-    this.elenentUser = document.querySelector(".greetingUser");
-    this.elenentSignIn = document.querySelector(".g_id_signin");
+    this.loginFormGoogle = this.rootElement.querySelector('#g_id_onload');
+    this.elenentUser =  this.rootElement.querySelector(".greetingUser");
+    this.elenentSignIn =  this.rootElement.querySelector(".g_id_signin");
 
     this.loggedUser = {
       googleID: '',
@@ -52,44 +52,15 @@ class GoogleLogin {
       UIT: ''
     };
 
-
     this.loginFormGoogle.dataset['client_id'] = "355085333852-8pr4q546m8hcdo2896mrc6ahq2brdaug.apps.googleusercontent.com";
     this.loginFormGoogle.dataset['callback'] = "handleCredentialResponse";
-
-
-
-    this.decodeJwtResponse = function (token) {
-      var base64Url = token.split(".")[1];
-      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      var jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split("")
-          .map(function (c) {
-            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-          })
-          .join("")
-      );
-      return JSON.parse(jsonPayload);
-    };
-
-    this.handleCredentialResponse = function (response) {
-      const responsePayload = this.decodeJwtResponse(response.credential);
-      this.loggedUser.googleID = responsePayload.sub;
-      this.loggedUser.name = responsePayload.given_name;
-      this.loggedUser.email = responsePayload.email;
-      this.loggedUser.UIT = responsePayload.jti;
-      this.elenentUser.onload;
-
-      this.elenentUser.innerText = this.loggedUser.name;
-
-
-    };
-
+    this.loginFormGoogle.callback = function(){
+      console.log(this)
+    }
   }
 
-
-
 }
+
 GoogleLogin.prototype.decodeJwtResponse = function (token) {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -103,8 +74,9 @@ GoogleLogin.prototype.decodeJwtResponse = function (token) {
   );
   return JSON.parse(jsonPayload);
 };
+
 GoogleLogin.prototype.handleCredentialResponse = function (response) {
-  const responsePayload = this.decodeJwtResponse(response.credential);
+  const responsePayload = this.prototype.decodeJwtResponse(response);
   this.loggedUser.googleID = responsePayload.sub;
   this.loggedUser.name = responsePayload.given_name;
   this.loggedUser.email = responsePayload.email;
@@ -119,5 +91,5 @@ GoogleLogin.prototype.handleCredentialResponse = function (response) {
 const gLogin = new GoogleLogin(".container.mx-auto.form-login");
 
 function handleCredentialResponse(response) {
-  gLogin.handleCredentialResponse(response);
+  gLogin.prototype.handleCredentialResponse(response);
 }
